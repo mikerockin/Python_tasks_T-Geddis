f334bac8440ca8c6563769f1ec61b2a62351ef56
Mikhail <mrmikhail88@gmail.com>
2021-09-10 09:17:56 +0500

    57

--- 57_rain_precipitation.py
+++ 57_rain_precipitation.py
@@ -1,10 +1,15 @@
 years = int(input('Введите количество лет : '))
 months = 12
 precipitation_all = 0
-print('Введите количество осадков в каждом месяце начиная с января.... ')
+call = 1
+print('Введите количество осадков в каждом месяце начиная с января.. ')
+print('№ месяца\tКоличество осадков')
+print('____________________________')
 for x in range(years):
-    for b in range(months):
+    for b in range(1, months + 1):
         precipitation = int(input('Введите кол-осадков в мм : '))
         precipitation_all += precipitation
         print(precipitation_all)
-        print(months)
+        print(b, '\t', precipitation_all)
+
+
