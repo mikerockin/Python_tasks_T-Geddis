2a6a4c7b4cb9a7af2dd2a84dbf4ef85cf0cab221
Mikhail <mrmikhail88@gmail.com>
2021-09-22 09:52:03 +0500

    upgraded  100

--- 2342323.py
+++ 2342323.py
@@ -1,3 +1,8 @@
-import random
-
-print(random.random())
+while name == name_user:
+    print('Ваш выбор:', name_user)
+    print('Выбор компьютера: ', name)
+    print('Ничья, переигрываем')
+    x = random.randint(1, 3)
+    print(x)
+    name = computer(x)
+    name_user = input('Камень? Ножницы? Бумага? ')
